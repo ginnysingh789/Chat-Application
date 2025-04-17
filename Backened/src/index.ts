7209347822aa2d05dbfbb5358b4cb878240e4387
ginnysingh789 <ginnysangral786@gmail.com>
2025-04-17 22:30:56 +0530

    update

--- Backened/src/index.ts
+++ Backened/src/index.ts
@@ -1,17 +1,64 @@
-import { WebSocketServer } from "ws";
-let userCount=0;
-let allSocket=[];
-const wss=new WebSocketServer({port:8000});
-wss.on("connection",(socket)=>{
-    allSocket.push(socket);
-    userCount=userCount+1;
-    socket.onmessage=(e)=>{ 
-       for(let s of allSocket)
-       {
-        if(s!==socket)
-        {
-            s.send(e.data.toString())
-        }
-       }
+import { Socket } from "dgram";
+import { WebSocketServer,WebSocket } from "ws";
+const wss = new WebSocketServer({ port: 5000 })
+
+interface userScheme{
+    socket:WebSocket,
+    roomId:string
+}
+let allConnectedUser:userScheme[]=[]
+try {
+    wss.on("connection", function (socket) {    
+       
+        //First Join the room
+        socket.on("message",(message)=>{
+            try {
+                const parsedObject=JSON.parse(message as unknown as string)//Web socket recieve only string
+            if(parsedObject.type==="join")
+            {
+                allConnectedUser.push({socket:socket,roomId:parsedObject.payload.roomId})
+                console.log("User Connected")
+            }
+            //After sending the room send the message
+            if(parsedObject.type==="chat")
+            {
+                //first get the user which send the message
+                let CurrentUserRoom=null
+                for(let i =0;i<allConnectedUser.length;i++)//Check user exist or not
+                {
+                    if(allConnectedUser[i].socket===socket)
+                    {
+                        CurrentUserRoom=allConnectedUser[i].roomId;
+                        console.log(CurrentUserRoom)
+                    }
+                }
+                if(CurrentUserRoom!=null)
+                {
+                    for(let i =0;i<allConnectedUser.length;i++)
+                    {
+                        if(CurrentUserRoom==allConnectedUser[i].roomId){
+                            allConnectedUser[i].socket.send(parsedObject.payload.message)
+                        }
+                    }
+                }
+                else
+                {
+                    console.log("Entry not found ");
+                }
+
+            }
+                
+            } catch (error) {
+                console.log(error)
+                
+            }
+            
+        })
+    })
+
+    } catch (error) {
+        console.log(error)
+
     }
-})
+
+
