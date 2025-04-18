4e5d043ed5e81f36e71fa87718a89cf042cff9e8
ginnysingh789 <ginnysangral786@gmail.com>
2025-04-18 20:00:08 +0530

    Frontend push

--- Frontened/src/Components/HomePage.tsx
+++ Frontened/src/Components/HomePage.tsx
@@ -1,31 +1,33 @@
-import { useState } from "react";
-import { Button } from "./Button";
+
 import { Card } from "./Card";
-import { Modal } from "./Modal";
-
-export const HomePage=()=> {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <Card>
-        <div className="flex flex-col items-center space-y-4">
-          <Button label="Join a Room" onClick={() => setIsModalOpen(true)} />
-          <Button label="Create a Room" />
+import { Button } from "./Button";
+import { useState } from "react";
+export const HomePage = () => {
+    const[createCode,setcreateCode]=useState<string|null>(null);
+    const randomNum=()=>{
+        const value:string=(Math.random().toString(36).substring(2,7));
+        setcreateCode(value)
+        
+    }
+    return (
+        <div className="flex">
+            <Card><div className="flex justify-center  text-2xl">Chat Application</div>
+                <div className=" flex flex-col mt-8 items-center">
+                    <div className="mt-1.5">
+                    <Button label="Join a Room "></Button>
+                    </div>
+                    <div className="mt-1.5">
+                    <Button label="Create a Room" onClick={randomNum} ></Button>
+                    </div>
+                </div>
+                <div >{(createCode!=null)?"Random value"+createCode:null}</div>
+            </Card>
+           
+            
         </div>
-      </Card>
 
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <h2 className="text-lg font-bold mb-4">Join a Room</h2>
-        <input
-          type="text"
-          placeholder="Enter Room Code"
-          className="w-full px-4 py-2 border rounded mb-4"
-        />
-        <Button label="Join" onClick={() => alert("Joining...")} />
-      </Modal>
-    </div>
-  );
-}
+
+    );
+};
 
 
